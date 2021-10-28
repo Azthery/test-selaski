8c6b18cd00d93e26e41db8ca49d0e8f45f7c4fcf
Azthery <aztheryy@gmail.com>
2021-10-28 11:46:48 -0300

    header fixed

--- frontend/src/app/shared/components/header/header.component.ts
+++ frontend/src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/core/services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  singout():void{
+    this.authService.signout();
+  }
+  logged(): boolean{
+    return this.authService.logged();
   }
 
 }
